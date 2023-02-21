41484f8b3f3b9225b272ffebd58fcced287f2a97
Oleg Gaiko <olegforsage@mail.ru>
2023-02-21 08:59:50 +0300

    last

--- src/PortTest.java
+++ src/PortTest.java
@@ -1,4 +1,32 @@
-import static org.junit.jupiter.api.Assertions.*;
-class PortTest {
-  
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import static junit.framework.TestCase.assertEquals;
+
+
+public class PortTest {
+    @Test
+    public void testParseIndexes() {
+        Port port = new Port(new String[]{"1,3-5", "2", "3-4"});
+        List<List<Integer>> expected = Arrays.asList(
+                Arrays.asList(1, 3, 4, 5),
+                Arrays.asList(2),
+                Arrays.asList(3, 4)
+        );
+        assertEquals(expected, port.parseIndexes());
+    }
+
+    @Test
+    public void testGetOrderedPairs() {String[] indexes = {"1-2", "5", "7-8"};
+        Port port = new Port(indexes);
+        List<List<Integer>> expected = new ArrayList<>();
+        expected.add(Arrays.asList(1, 5, 7));
+        expected.add(Arrays.asList(1, 5, 8));
+        expected.add(Arrays.asList(2, 5, 7));
+        expected.add(Arrays.asList(2, 5, 8));
+        List<List<Integer>> actual = port.getOrderedPairs();
+        assertEquals(expected, actual);
+    }
 }
